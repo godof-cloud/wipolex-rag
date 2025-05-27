7b47aab81aca6904b32f4454acb46eb7331c54dd
jaguilop <javier.aguilar@wipo.int>
2025-05-27 14:13:34 +0200

    latest changes

--- back-end/src/main/java/org/wipo/wipolex/rag/Application.java
+++ back-end/src/main/java/org/wipo/wipolex/rag/Application.java
@@ -1,17 +1,22 @@
 package org.wipo.wipolex.rag;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.model.bedrock.titan.autoconfigure.BedrockTitanEmbeddingAutoConfiguration;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		BedrockTitanEmbeddingAutoConfiguration.class
+	})
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
 	ChatClient chatClient(ChatClient.Builder builder, VectorStore vectorStore) {
 		var systemPrompt = """
 				Your are an AI powered lawyer that helps users to find useful information about ...
